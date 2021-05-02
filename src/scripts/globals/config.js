302a67de1206f492104f04184ee2a079861d6d87
anggastudio <aa.angga.pratama@gmail.com>
2021-05-03 01:37:58 +0700

    enhance performance

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
-  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
-  BASE_IMAGE_URL_LARGE: 'https://restaurant-api.dicoding.dev/images/large/',
+  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
+  BASE_IMAGE_URL_LARGE: 'https://restaurant-api.dicoding.dev/images/medium/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: new Date().toISOString(),
   DATABASE_NAME: 'aa-restos-database',
